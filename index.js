dbc6d14f6a0e0530ea0451e774c000f4bcdebd39
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2017-03-29 16:48:47 +0200

    Adding bracket

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const blue = chalk.blue
 const excelbuilder = require('msexcel-builder');
 _.mixin(require("lodash-deep"));
 
-let jsonToExtract = fs.readJsonSync(__dirname + '/json/file.json')
+let jsonToExtract = fs.readJsonSync(__dirname + '/json/components.json')
 
 let paths = []
 let tags = []
@@ -24,7 +24,7 @@ _.deepMapValues(jsonToExtract, function(value, path){
   toTranslate.push(value)
 });
 
-let workbook = excelbuilder.createWorkbook('./xlsx', 'sample.xlsx')
+let workbook = excelbuilder.createWorkbook('./xlsx', 'components.xlsx')
 
 let sheet1 = workbook.createSheet('sheet1', 3, paths.length + 2);
 
@@ -36,13 +36,17 @@ sheet1.set(3, 1, 'Target')
 _.each(paths, function(value, key) {
   let rows = key + 2
 
-  let goodPaths = paths[key].replace(/\.([0-9]*)\./g, '[$1].')
+  let goodPaths = paths[key].replace(/^([0-9]*)\./g, '[$1].')
+  goodPaths = goodPaths.replace(/\.([0-9]*)\./g, '[$1].')
+
+  if(toTranslate[key] === null) {
+    toTranslate[key] = 'null'
+  }
 
   sheet1.set(1, rows, goodPaths)
-  sheet1.set(2, rows, tags[key])
-  sheet1.set(3, rows, Entities.decode(toTranslate[key]))
+  sheet1.set(2, rows, Entities.decode(toTranslate[key].toString()))
+  sheet1.set(3, rows, Entities.decode(toTranslate[key].toString()))
 })
-
 
  // Save it
  workbook.save(function(err){
