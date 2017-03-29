14f9186323ec213c236a9e223d0dbe087df157ba
Cédrick ALEXANDRE <alexandre.cedrick@gmail.com>
2017-03-29 18:36:30 +0200

    Adding config

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ const green = chalk.green
 const grey = chalk.grey
 const blue = chalk.blue
 
-
 const excelbuilder = require('msexcel-builder');
+
+const checkFileExistsSync = require('./tools/checkFileExistsSync')
+
 _.mixin(require("lodash-deep"));
 
 let jsonToExtract = fs.readJsonSync(__dirname + '/json/components.json')
+
+let config = fs.readJsonSync(__dirname + '/config/components.conf')
+
+// Make filter
+let reg = config.exception.join('|')
+var regex = new RegExp(reg,"g");
 
 let paths = []
 let tags = []
@@ -30,22 +38,36 @@ let sheet1 = workbook.createSheet('sheet1', 3, paths.length + 2);
 
 // Fill some data
 sheet1.set(1, 1, 'Reference')
+sheet1.width(1, 25)
 sheet1.set(2, 1, 'Source')
+sheet1.width(2, 50)
 sheet1.set(3, 1, 'Target')
+sheet1.width(3, 50)
 
+
+let resetRow = 1
 _.each(paths, function(value, key) {
   let rows = key + 2
 
   let goodPaths = paths[key].replace(/^([0-9]*)\./g, '[$1].')
   goodPaths = goodPaths.replace(/\.([0-9]*)\./g, '[$1].')
+  goodPaths = goodPaths.replace(/(\.([0-9]))/g, '.[$2]')
 
-  if(toTranslate[key] === null) {
-    toTranslate[key] = 'null'
+  let test = regex.test(goodPaths)
+  if(test) {
+    console.log(green(test + ' ' + goodPaths))
+
+    if(toTranslate[key] === null) {
+      toTranslate[key] = 'null'
+    }
+
+    sheet1.set(1, rows - resetRow, goodPaths)
+    sheet1.set(2, rows - resetRow, Entities.decode(toTranslate[key].toString()))
+    sheet1.set(3, rows - resetRow, Entities.decode(toTranslate[key].toString()))
+
+  } else {
+    resetRow++
   }
-
-  sheet1.set(1, rows, goodPaths)
-  sheet1.set(2, rows, Entities.decode(toTranslate[key].toString()))
-  sheet1.set(3, rows, Entities.decode(toTranslate[key].toString()))
 })
 
  // Save it
