90b72dca5b77ce2cdb6f51ef501dbee08fb8dbdd
cedrick alexandre <alexandre.cedrick@gmail.com>
2017-04-24 16:58:53 +0100

    Fix empty language directory

--- commands/populate.js
+++ commands/populate.js
@@ -7,6 +7,7 @@ const red = chalk.red
 const green = chalk.green
 const grey = chalk.grey
 const blue = chalk.blue
+const yellow = chalk.yellow
 
 const inquirer = require('inquirer')
 const path = require('path')
@@ -40,43 +41,46 @@ function populate (language) {
     folders = _.filter(folders, function(folder) {
       let pathFolder = path.join(workFolderXLSX, folder)
       if(!fs.statSync(pathFolder).isDirectory()) return false
-      console.log(pathFolder)
       let files = fs.readdirSync(pathFolder)
       if(files.length) return true
     })
-    console.log('me')
 
-    inquirer.prompt({
-      type: 'list',
-      name: 'folder',
-      message: 'Which language?',
-      choices: folders
-    }).then(function (answers) {
-      let files = fs.readdirSync(path.join(workFolderXLSX, answers.folder))
-      let extractFile = path.join(workFolderXLSX, answers.folder, files[0])
-      let jsonTradFolder = path.join(workFolderJSON, answers.folder)
+    if(folders.length !== 0) {
+      inquirer.prompt({
+        type: 'list',
+        name: 'folder',
+        message: 'Which language?',
+        choices: folders
+      }).then(function (answers) {
+        let files = fs.readdirSync(path.join(workFolderXLSX, answers.folder))
+        let extractFile = path.join(workFolderXLSX, answers.folder, files[0])
+        let jsonTradFolder = path.join(workFolderJSON, answers.folder)
 
-      const workbook = XLSX.readFile(extractFile)
-      const sheetNameList = workbook.SheetNames
+        const workbook = XLSX.readFile(extractFile)
+        const sheetNameList = workbook.SheetNames
 
-      _.each(sheetNameList, function(JSONsheet, key) {
-        console.time('populate-' + JSONsheet)
-        let JSONsheetdata = XLSX.utils.sheet_to_json(workbook.Sheets[JSONsheet])
+        _.each(sheetNameList, function(JSONsheet, key) {
+          console.time('populate-' + JSONsheet)
+          let JSONsheetdata = XLSX.utils.sheet_to_json(workbook.Sheets[JSONsheet])
 
-        let JSONFilePathOri = path.join(workFolderJSONOri, JSONsheet + '.json')
-        // let JSONfile = path.join(JSONFilePathOri)
-        let JSONfile = fs.readJsonSync(JSONFilePathOri, 'UTF-8')
+          let JSONFilePathOri = path.join(workFolderJSONOri, JSONsheet + '.json')
+          // let JSONfile = path.join(JSONFilePathOri)
+          let JSONfile = fs.readJsonSync(JSONFilePathOri, 'UTF-8')
 
-        _.each(JSONsheetdata, function (value) {
-          nestedProperty.set(JSONfile, value.Reference, value.Target)
+          _.each(JSONsheetdata, function (value) {
+            nestedProperty.set(JSONfile, value.Reference, value.Target)
+          })
+
+          fs.copySync(JSONFilePathOri, path.join(jsonTradFolder, JSONsheet + '.json'))
+
+          fs.writeJSONSync(path.join(jsonTradFolder, JSONsheet + '.json'), JSONfile)
+          console.timeEnd('populate-' + JSONsheet)
         })
-
-        fs.copySync(JSONFilePathOri, path.join(jsonTradFolder, JSONsheet + '.json'))
-
-        fs.writeJSONSync(path.join(jsonTradFolder, JSONsheet + '.json'), JSONfile)
-        console.timeEnd('populate-' + JSONsheet)
       })
-    })
+    } else {
+      console.log(yellow('Please don\'t forget to put a file in language directory'))
+    }
+
   })
 }
 
