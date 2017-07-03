15a8c10cb60443ec726b194f1c5ceeec900d6b2b
cedrick alexandre <alexandre.cedrick@gmail.com>
2017-07-03 17:17:45 +0100

    Command duplicateRemove done V1.0.0

--- commands/removeDuplicate.js
+++ commands/removeDuplicate.js
@@ -36,36 +36,75 @@ function removeDuplicate () {
   _.each(sheetNameList, function (JSONsheet, key) {
     let JSONsheetdata = XLSX.utils.sheet_to_json(workbook.Sheets[JSONsheet])
 
-    let duplicate = ""
+    let duplicate = ''
     let duplicateObject = {}
+    let resetRow = 0
     _.each(JSONsheetdata, function (data, key) {
       if(JSONsheet === "components") {
-        if(key > 0) {
-          if(data.Target !== "true" && data.Target !== "false") {
+        let valueReference = ''
+        let arrayReference = []
 
-            if(data.Target === JSONsheetdata[key - 1].Target) {
-              if(duplicate === "") {
-                duplicate = JSONsheetdata[key - 1].Reference + ","+ data.Reference
-              } else {
-                duplicate += "," + data.Reference
-              }
+        if(key === 0) {
+          if(data.Target === JSONsheetdata[key + 1].Target) {
+            // duplicate = data.Reference
+          } else {
+            sheet1.set(1, key + 2 - resetRow, data.Reference)
+            sheet1.set(2, key + 2 - resetRow, data.Source)
+            sheet1.set(3, key + 2 - resetRow, data.Target)
+            sheet1.set(4, key + 2 - resetRow, data.Clean)
+          }
+        } else if(key > 0 && key < JSONsheetdata.length - 1) {
+          valueReference = data.Target
+
+          if(valueReference === JSONsheetdata[key + 1].Target) {
+            // Contact ref plus ref next
+            if(duplicate !== ''){
+              duplicate = duplicate + ',' + JSONsheetdata[key + 1].Reference
             } else {
-              duplicate = ""
-              // console.log(data.Target)
+              duplicate = data.Reference + ',' + JSONsheetdata[key + 1].Reference
             }
-
-
-            if(duplicate !== "") {
-              duplicateObject = {path: duplicate, value: data.Target}
-              sheet1.set(1, key + 2, duplicateObject.path)
+            resetRow++
+            console.log(resetRow)
+          } else {
+            if(duplicate !== '') {
+              console.log(duplicate)
+              sheet1.set(1, key + 2 - resetRow, duplicate)
+              sheet1.set(2, key + 2 - resetRow, data.Source)
+              sheet1.set(3, key + 2 - resetRow, valueReference)
+              sheet1.set(4, key + 2 - resetRow, data.Clean)
             } else {
-              sheet1.set(1, key + 2, data.Reference)
+              sheet1.set(1, key + 2 - resetRow, data.Reference)
+              sheet1.set(2, key + 2 - resetRow, data.Source)
+              sheet1.set(3, key + 2 - resetRow, data.Target)
+              sheet1.set(4, key + 2 - resetRow, data.Clean)
             }
-            sheet1.set(2, key + 2, data.Source)
-            sheet1.set(3, key + 2, data.Target)
-            sheet1.set(4, key + 2, data.Clean)
+            valueReference = ''
+            duplicate = ''
+            // sheet1.set(1, key + 2, data.Reference)
+            // sheet1.set(2, key + 2, data.Source)
+            // sheet1.set(3, key + 2, data.Target)
+            // sheet1.set(4, key + 2, data.Clean)
+          }
+
+          // if(data.Target === valueReference)
+        } else {
+          if(duplicate !== '') {
+            console.log(duplicate)
+            sheet1.set(1, key + 2 - resetRow, duplicate)
+            sheet1.set(2, key + 2 - resetRow, data.Source)
+            sheet1.set(3, key + 2 - resetRow, valueReference)
+            sheet1.set(4, key + 2 - resetRow, data.Clean)
           }
         }
+
+        // if(data.Target === JSONsheetdata[key - 1].Target && data.Target === JSONsheetdata[key + 1].Target) {
+        //   console.log("f")
+        // }
+
+        // sheet1.set(1, key + 2, data.Reference)
+        // sheet1.set(2, key + 2, data.Source)
+        // sheet1.set(3, key + 2, data.Target)
+        // sheet1.set(4, key + 2, data.Clean)
       }
     })
 
@@ -75,7 +114,7 @@ function removeDuplicate () {
     if (err) throw err
     else {
       console.log(green('extract.xlsx') + ' was created')
-      console.timeEnd('extract')
+      // console.timeEnd('extract')
     }
   })
 
